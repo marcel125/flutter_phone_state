fc11f938b22231aebc23fa89e6ec200fefae0506
Vishnu <vishnu@insight.co>
2024-03-04 16:44:07 +0530

    Fixed issue for Android

--- example/android/app/src/main/kotlin/co/sunnyapp/flutter_phone_state_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/co/sunnyapp/flutter_phone_state_example/MainActivity.kt
@@ -1,8 +1,6 @@
 package co.sunnyapp.flutter_phone_state_example
 
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
    
